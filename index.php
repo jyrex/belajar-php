1d03f051ed666683648d53b12987569af4734cef
jyrex <jeffer.maliangkay@gmail.com>
2017-02-24 20:27:47 +0700

    new hendrity

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// echo "hello muhlas";
+
+$nama = "nama saya";
+$umur = 33;
+$ipk = 3.96;
+
+echo "halo namaku $nama".' umur temanku $umur '." ipk dia ".$ipk."<br>";
+
+if ($ipk === 3.96){
+	echo "hendrity sejati";
+} else {
+	echo "cewa la"
+}
+
+?>
